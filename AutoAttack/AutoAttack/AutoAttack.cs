3c96ae2cd77d727bf50d3bbee574bc8ca8a8157b
unknown <mandha1@student.op.ac.nz>
2020-12-06 00:52:41 +1300

    now detects npc and makes em jump

--- AutoAttack/AutoAttack/AutoAttack.cs
+++ AutoAttack/AutoAttack/AutoAttack.cs
@@ -17,11 +17,9 @@ namespace AutoAttack
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
-
             helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+            
         }
-
 
         /*********
         ** Private methods
@@ -29,17 +27,6 @@ namespace AutoAttack
         /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
-
-        }
-
         private void OnUpdateTicked(object sender, EventArgs e)
         {
             // ignore if player hasn't loaded a save yet
@@ -50,9 +37,59 @@ namespace AutoAttack
 
             // facing up = 0, right = 1, down = 2, left = 3
             this.Monitor.Log($"X: {Math.Floor(Game1.player.Position.X / Game1.tileSize)} Y: {Math.Floor(Game1.player.Position.Y / Game1.tileSize)}. Facing: {Game1.player.FacingDirection}", LogLevel.Debug);
-
-
             //this.Monitor.Log($"Location X: {Game1.player.getTileX() * Game1.tileSize} Y: {Game1.player.getTileY() * Game1.tileSize}.", LogLevel.Debug);
+
+            double PlayerX = Math.Floor(Game1.player.Position.X / Game1.tileSize);
+            double PlayerY = Math.Floor(Game1.player.Position.Y / Game1.tileSize);
+            double PlayerDirection = Game1.player.FacingDirection;
+
+            GameLocation gameLocation = Game1.player.currentLocation;
+            
+            double tileInFront = 0;
+
+            switch (PlayerDirection)
+            {
+                //  Player is facing up
+                case 0:
+                    //-1y from player pos
+                    tileInFront = PlayerY - 1;
+
+                    Microsoft.Xna.Framework.Point p = new Microsoft.Xna.Framework.Point(Convert.ToInt32(PlayerX), Convert.ToInt32(tileInFront));
+
+                    foreach (Character c in gameLocation.characters)
+                        if (c.getTileLocationPoint() == p)
+                        {
+                            c.jumpWithoutSound();
+                            this.Monitor.Log($"{c.name}", LogLevel.Debug);
+                        }
+
+                    break;
+
+                //  Player is facing right
+                case 1:
+                    //tileInFront = PlayerX + 1;
+                    break;
+
+                //  Player is facing down
+                case 2:
+                    tileInFront = PlayerY + 1;
+
+                    p = new Microsoft.Xna.Framework.Point(Convert.ToInt32(PlayerX), Convert.ToInt32(tileInFront));
+
+                    foreach (Character c in gameLocation.characters)
+                        if (c.getTileLocationPoint() == p)
+                        {
+                            c.jumpWithoutSound();
+                            this.Monitor.Log($"{c.name} {c.IsMonster.ToString()}", LogLevel.Debug);
+                        }
+                    break;
+
+                //  Player is facing left
+                case 3:
+                    //-1x from player pos
+                    //tileInFront = PlayerX - 1;
+                    break;
+            }
         }
     }
 }
