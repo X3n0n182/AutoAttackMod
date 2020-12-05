4e258b123f3e42f08ed19e8355ee55cc4cac930b
unknown <mandha1@student.op.ac.nz>
2020-12-06 12:44:45 +1300

    mod detecting npc in front properly

--- AutoAttack/AutoAttack/AutoAttack.cs
+++ AutoAttack/AutoAttack/AutoAttack.cs
@@ -17,8 +17,7 @@ namespace AutoAttack
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
-            
+            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;           
         }
 
         /*********
@@ -35,50 +34,64 @@ namespace AutoAttack
 
             // tile poisition in the current level
 
-            // facing up = 0, right = 1, down = 2, left = 3
-            this.Monitor.Log($"X: {Math.Floor(Game1.player.Position.X / Game1.tileSize)} Y: {Math.Floor(Game1.player.Position.Y / Game1.tileSize)}. Facing: {Game1.player.FacingDirection}", LogLevel.Debug);
-            //this.Monitor.Log($"Location X: {Game1.player.getTileX() * Game1.tileSize} Y: {Game1.player.getTileY() * Game1.tileSize}.", LogLevel.Debug);
-
             double PlayerX = Math.Floor(Game1.player.Position.X / Game1.tileSize);
             double PlayerY = Math.Floor(Game1.player.Position.Y / Game1.tileSize);
+
             double PlayerDirection = Game1.player.FacingDirection;
 
             GameLocation gameLocation = Game1.player.currentLocation;
-            
+            Tool tool;
+
             double tileInFront = 0;
+
+            // facing up = 0, right = 1, down = 2, left = 3
+
+            this.Monitor.Log($"X: {PlayerX} Y: {PlayerY}, Facing: {PlayerDirection}", LogLevel.Debug);
+
+            //this.Monitor.Log($"X: {Math.Floor(Game1.player.Position.X / Game1.tileSize)} Y: {Math.Floor(Game1.player.Position.Y / Game1.tileSize)}. Facing: {Game1.player.FacingDirection}", LogLevel.Debug);
+            //this.Monitor.Log($"Location X: {Game1.player.getTileX() * Game1.tileSize} Y: {Game1.player.getTileY() * Game1.tileSize}.", LogLevel.Debug);
 
             switch (PlayerDirection)
             {
                 //  Player is facing up
                 case 0:
                     //-1y from player pos
-                    tileInFront = PlayerY - 1;
+                    PlayerY = PlayerY - 1;
                     break;
 
                 //  Player is facing right
                 case 1:
-                    tileInFront = PlayerX + 1;
+                    PlayerX = PlayerX + 1;
                     break;
 
                 //  Player is facing down
                 case 2:
-                    tileInFront = PlayerY + 1;
+                    PlayerY = PlayerY + 1;
                     break;
 
                 //  Player is facing left
                 case 3:
-                    tileInFront = PlayerX - 1;
+                    PlayerX = PlayerX - 1;
                     break;
             }
-
-            Microsoft.Xna.Framework.Point p = new Microsoft.Xna.Framework.Point(Convert.ToInt32(PlayerX), Convert.ToInt32(tileInFront));
-
+           
             foreach (Character c in gameLocation.characters)
-                while ((c.getTileLocationPoint() == p) && c.IsMonster == true)
+            {
+                int npcX = Convert.ToInt32(Math.Floor(c.Position.X / Game1.tileSize));
+                int npcY = Convert.ToInt32(Math.Floor(c.Position.Y / Game1.tileSize));
+
+                //  Farmer position +1 tile whatever direction they're facing
+                Point p = new Point(Convert.ToInt32(PlayerX), Convert.ToInt32(PlayerY));
+
+                //  NPC position
+                Point p2 = new Point(npcX, npcY);
+
+                if (p == p2 && Game1.player.IsEquippedItem(tool. ))
                 {
-                    c.jumpWithoutSound();
-                    this.Monitor.Log($"Attacking monster {c.name}", LogLevel.Debug);
+                    Game1.player.
+                    this.Monitor.Log($"{c.name} is in front of you.", LogLevel.Debug);
                 }
+            }                
         }
     }
 }
