a1ffcd81eca3318c660d2ea60c1ca9d7fe3adbae
unknown <mandha1@student.op.ac.nz>
2020-12-08 20:35:27 +1300

    player now turns and faces monster, robustness needs work

--- AutoAttack/AutoAttack/AutoAttack.cs
+++ AutoAttack/AutoAttack/AutoAttack.cs
@@ -19,60 +19,34 @@ namespace AutoAttack
 
         private void OnUpdateTicked(object sender, EventArgs e)
         {
-            // ignore if player hasn't loaded a save yet
             if (!Context.IsWorldReady)
                 return;
 
             Farmer player = Game1.player;
             Tool playerTool = Game1.player.CurrentTool;     
             GameLocation playerLocation = Game1.player.currentLocation;
-            List<Vector2> tileRange = new List<Vector2>();
 
-            switch (player.getFacingDirection())
-            {
-                // up right down left
-                case 0:
-                    tileRange.Add(new Vector2(player.getTileX(), player.getTileY() - 1));           // 🟩 🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX(), player.getTileY() - 2));           // 🟩 🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() - 1));       //   O
-                    tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() - 1));
-                    tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() - 2));
-                    tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() - 2));
-                    break;
-                case 1:
-                    tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY()));
-                    tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() - 1));       //   🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() + 1));       // O 🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX() + 2, player.getTileY()));           //   🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX() + 2, player.getTileY() - 1));
-                    tileRange.Add(new Vector2(player.getTileX() + 2, player.getTileY() + 1));       
-                    break;
-                case 2:
-                    tileRange.Add(new Vector2(player.getTileX(), player.getTileY() + 1));           //   O
-                    tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() + 1));       // 🟩 🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() + 1));       // 🟩 🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX(), player.getTileY() - 2));
-                    tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() - 2));
-                    tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() - 2));
-                    break;
-                case 3:
-                    tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY()));           // 🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() - 1));       // 🟩 🟩 O
-                    tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() + 1));       // 🟩 🟩
-                    tileRange.Add(new Vector2(player.getTileX() - 2, player.getTileY()));
-                    tileRange.Add(new Vector2(player.getTileX() - 2, player.getTileY() - 1));
-                    tileRange.Add(new Vector2(player.getTileX() - 2, player.getTileY() + 1));
-                    break;
-            }
+            const int TILERANGE = 2;
+                  
+            Character c = Utility.isThereAFarmerOrCharacterWithinDistance(player.getTileLocation(), TILERANGE, playerLocation);
 
-            foreach (Character c in playerLocation.characters)
+            if (c.IsMonster == true && c != player && playerTool is MeleeWeapon)
             {
-                if (c.IsMonster == false && c != player && c.name == "Keeper")
+                if (c.getTileLocation() == player.getTileLocation())
                 {
-                    player.faceDirection(player.getGeneralDirectionTowards(c.getTileLocation()));
-                    //player.FacingDirection = player.getGeneralDirectionTowards(c.getTileLocation());
+                    MeleeWeapon mw = new MeleeWeapon();
+                    mw.triggerDefenseSwordFunction(player);
+
                 }
+                else
+                {
+                    player.FacingDirection = player.getGeneralDirectionTowards(c.Position);
+                    playerTool.leftClick(player);
+                }
+                
             }
+
+            c = null;
         }
 
         //private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
@@ -83,7 +57,49 @@ namespace AutoAttack
 
         //    // print button presses to the console window
         //    this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
-            
+
         //}
+
+        private void unusedCode()
+        {
+            //List<Vector2> tileRange = new List<Vector2>();
+            //
+            //switch (player.getFacingDirection())
+            //{
+            //    // up right down left
+            //    case 0:
+            //        tileRange.Add(new Vector2(player.getTileX(), player.getTileY() - 1));           // 🟩 🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX(), player.getTileY() - 2));           // 🟩 🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() - 1));       //   O
+            //        tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() - 1));
+            //        tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() - 2));
+            //        tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() - 2));
+            //        break;
+            //    case 1:
+            //        tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY()));
+            //        tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() - 1));       //   🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() + 1));       // O 🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX() + 2, player.getTileY()));           //   🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX() + 2, player.getTileY() - 1));
+            //        tileRange.Add(new Vector2(player.getTileX() + 2, player.getTileY() + 1));
+            //        break;
+            //    case 2:
+            //        tileRange.Add(new Vector2(player.getTileX(), player.getTileY() + 1));           //   O
+            //        tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() + 1));       // 🟩 🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() + 1));       // 🟩 🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX(), player.getTileY() - 2));
+            //        tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() - 2));
+            //        tileRange.Add(new Vector2(player.getTileX() + 1, player.getTileY() - 2));
+            //        break;
+            //    case 3:
+            //        tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY()));           // 🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() - 1));       // 🟩 🟩 O
+            //        tileRange.Add(new Vector2(player.getTileX() - 1, player.getTileY() + 1));       // 🟩 🟩
+            //        tileRange.Add(new Vector2(player.getTileX() - 2, player.getTileY()));
+            //        tileRange.Add(new Vector2(player.getTileX() - 2, player.getTileY() - 1));
+            //        tileRange.Add(new Vector2(player.getTileX() - 2, player.getTileY() + 1));
+            //        break;
+            //}
+        }
     }
 }
