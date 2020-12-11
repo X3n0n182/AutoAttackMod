711c1aaaa7e674e9277ee63efe4ea59470dce225
unknown <mandha1@student.op.ac.nz>
2020-12-12 00:38:13 +1300

    added comments

--- AutoAttack/AutoAttack/AutoAttack.cs
+++ AutoAttack/AutoAttack/AutoAttack.cs
@@ -10,13 +10,7 @@ using System.Collections.Generic;
 namespace AutoAttack
 {
     public class AutoAttack : Mod
-    {
-        const int DETECTIONRANGE = 4;
-
-        Farmer player = Game1.player;
-        Tool playerTool = Game1.player.CurrentTool;
-        GameLocation playerLocation = Game1.player.currentLocation;
-               
+    {           
         public override void Entry(IModHelper helper)
         {
             helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
@@ -27,22 +21,36 @@ namespace AutoAttack
             if (!Context.IsWorldReady)
                 return;
 
+            //  Tile range at which to start swinging your weapon towards the monster
+            const int DETECTIONRANGE = 4;
+
+            //  Get player, their equipped tool and current location on the map
+            Farmer player = Game1.player;
+            Tool playerTool = Game1.player.CurrentTool;
+            GameLocation playerLocation = Game1.player.currentLocation;
+
+            //  Get the closest NPC in range
             Character c = Utility.isThereAFarmerOrCharacterWithinDistance(player.getTileLocation(), DETECTIONRANGE, playerLocation);
 
             //  Start swinging at the character's direction if a monster is detected
             //      and a  melee weapon is selected
 
-            if (c.name == "Abigail" && c != player && playerTool is MeleeWeapon)      //debug aim detection using spouse lmao
-            //if (c.IsMonster == true && c != player && playerTool is MeleeWeapon)
+            //debug aim detection using spouse lmao
+            //if (c.name == "Penny" && c != player && playerTool is MeleeWeapon) 
+            
+            //  Point farmer towards farmer if it's a monster and farmer has equipped a melee weapon
+            if (c.IsMonster == true && c != player && playerTool is MeleeWeapon)
             {
+                //  Direction to point farmer towards monster
                 int direction = player.getGeneralDirectionTowards(c.Position);
 
+                //  Don't do anything if already facing monster
                 if (player.FacingDirection == direction) return;
                
                 // We moved away from the target; make farmer face the right way again
                 player.FacingDirection = direction;
 
-                // Set run animation to be heading towards the target to simulate side-stepping
+                // Set run animation to be pointing towards the target to simulate side-stepping
                 switch (direction)
                 {
                     case 0:
@@ -59,6 +67,7 @@ namespace AutoAttack
                         break;
                 }
 
+                //  Start swinging the weapon
                 playerTool.beginUsing(playerLocation, (int)player.GetToolLocation(true).X, (int)player.GetToolLocation(true).Y, player);
 
                 return;
