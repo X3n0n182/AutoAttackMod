337cc3e3038f56bd5e84457ac15308ec1500a639
unknown <mandha1@student.op.ac.nz>
2020-12-13 02:36:04 +1300

    fixed memory leak and freeze on pickaxe use bug

--- AutoAttack/AutoAttack/AutoAttack.cs
+++ AutoAttack/AutoAttack/AutoAttack.cs
@@ -18,60 +18,73 @@ namespace AutoAttack
 
         private void OnUpdateTicked(object sender, EventArgs e)
         {
+            //  Don't do stuff if player hasn't loaded a save
             if (!Context.IsWorldReady)
                 return;
 
             //  Tile range at which to start swinging your weapon towards the monster
             const int DETECTIONRANGE = 4;
 
-            //  Get player, their equipped tool and current location on the map
-            Farmer player = Game1.player;
-            Tool playerTool = Game1.player.CurrentTool;
+            //  Get player and current location on the map
+            Farmer player = Game1.player;            
             GameLocation playerLocation = Game1.player.currentLocation;
 
             //  Get the closest NPC in range
             Character c = Utility.isThereAFarmerOrCharacterWithinDistance(player.getTileLocation(), DETECTIONRANGE, playerLocation);
-
-            //  Start swinging at the character's direction if a monster is detected
-            //      and a  melee weapon is selected
-
+           
             //debug aim detection using spouse lmao
-            //if (c.name == "Penny" && c != player && playerTool is MeleeWeapon) 
+            //if (c.name == "Abigail" && c != player && playerTool is MeleeWeapon)
+
+            String[] mwTypes = { "sword", "club", "dagger" };
             
-            //  Point farmer towards farmer if it's a monster and farmer has equipped a melee weapon
-            if (c.IsMonster == true && c != player && playerTool is MeleeWeapon)
+            if (player.CurrentItem != null)
             {
-                //  Direction to point farmer towards monster
-                int direction = player.getGeneralDirectionTowards(c.Position);
+                Tool playerTool = Game1.player.CurrentTool;
 
-                //  Don't do anything if already facing monster
-                if (player.FacingDirection == direction) return;
-               
-                // We moved away from the target; make farmer face the right way again
-                player.FacingDirection = direction;
-
-                // Set run animation to be pointing towards the target to simulate side-stepping
-                switch (direction)
+                //  Start swinging at the character's direction if a monster is detected
+                //      and a proper melee weapon is selected
+                foreach (String s in mwTypes)
                 {
-                    case 0:
-                        player.FarmerSprite.animate(48, new GameTime());
-                        break;
-                    case 1:
-                        player.FarmerSprite.animate(40, new GameTime());
-                        break;
-                    case 2:
-                        player.FarmerSprite.animate(32, new GameTime());
-                        break;
-                    case 3:
-                        player.FarmerSprite.animate(56, new GameTime());
-                        break;
+                    if (playerTool.getCategoryName().ToLower().Contains(s))
+                    {
+                        //  Point farmer towards farmer if it's a monster and farmer has equipped a melee weapon
+                        if (c.IsMonster == true && c != player)
+                        {
+                            //  Direction to point farmer towards monster
+                            int direction = player.getGeneralDirectionTowards(c.Position);
+
+                            //  Don't do anything if already facing monster
+                            if (player.FacingDirection == direction) return;
+
+                            // We moved away from the target; make farmer face the right way again
+                            player.FacingDirection = direction;
+
+                            // Set run animation to be pointing towards the target to simulate side-stepping
+                            //  depending on set direction
+                            switch (direction)
+                            {
+                                case 0:
+                                    player.FarmerSprite.animate(48, new GameTime());
+                                    break;
+                                case 1:
+                                    player.FarmerSprite.animate(40, new GameTime());
+                                    break;
+                                case 2:
+                                    player.FarmerSprite.animate(32, new GameTime());
+                                    break;
+                                case 3:
+                                    player.FarmerSprite.animate(56, new GameTime());
+                                    break;
+                            }
+
+                            //  Start swinging the weapon
+                            playerTool.beginUsing(playerLocation, (int)player.GetToolLocation(true).X, (int)player.GetToolLocation(true).Y, player);
+
+                            return;
+                        }
+                    }
                 }
-
-                //  Start swinging the weapon
-                playerTool.beginUsing(playerLocation, (int)player.GetToolLocation(true).X, (int)player.GetToolLocation(true).Y, player);
-
-                return;
-            }           
+            }      
         }
     }
 }
